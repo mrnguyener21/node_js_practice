dd3c91e0abb67ee9868c6bf8003f66565d44b4c8
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-12-30 13:10:31 -0800

    updated notes to be taken in js file for better indentation and understanding

---
+++ Node_Notes_Practice_Etc/nodeModule.js
@@ -0,0 +1,39 @@
+// // OS MODULE 
+// // the os module lets you get information on your operation system(os)
+// // const os = require('os') // this is the most common way to set the os module to a variable
+// // lets you create things around your system
+
+// PATH
+// the path module is for editing the path to reach different files and getting different results from the path itself
+// you just have to require 'path' and from there you can access the different functions for the path module
+// ususally you just set it to a variable
+// const path = require('path')
+// 'C:\\temp\\myfile.html' is an example of what a path is. the path module will then edit around it
+
+// HTTP
+// you use HTTP in order to use an HTTP server
+// similar to other modules, you just require it and from there you can start using the different functions for it
+// const http = require('http')
+// you can do things like creating an HTTP port 
+// HTTP module also contains multiple classes in the module which has their own set of functions based on the goal of that class
+    // HTTP CLASSES:
+    // http.Agent
+        
+    // http.ClienRequest
+
+    // http.server
+
+    // http.ServerResponse
+
+    // http.IncomingMessage
+
+
+
+
+// FS
+
+// EVENTS
+
+// BUFFERS
+
+// STREAMS
