2ad542552fb35cd310f4a7c719066d8bd4cd5433
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-12-31 17:52:16 -0800

    went through and played around with starting servers and understand what mentor did and able to read the code, but not fully understand it/ comfortable with it yet

--- Node_Notes_Practice_Etc/practice.js
+++ Node_Notes_Practice_Etc/practice.js
@@ -1,14 +1,29 @@
 const { dir } = require('console')
 const fs = require('fs')
 const http = require('http')
+const express = require('express')
 //read a current file
-console.log(__dirname) //dirname gives you the absolute path so you don't need to do a bunch of ../../
-fs.readFile(`${__dirname}/practiceText.txt`, 'utf-8', (err, data) => {
-if (err){
-    throw new Error(err);
-}else {
-    console.log(data)
-}
-});
+// console.log(__dirname) //dirname gives you the absolute path so you don't need to do a bunch of ../../
+// fs.readFile(`${__dirname}/practiceText.txt`, 'utf-8', (err, data) => { //what's the point of utf-8
+// if (err){
+//     throw new Error(err);
+// }else {
+//     console.log(data)
+// }
+// });
 // console.log(readPracticeFile)
 //create and read a new file
+
+
+//create a simple html erver
+const host = 'localhost'
+const port = 8000
+const server = http.createServer((req,res) => {
+    console.log('req',req,res)
+})
+
+server.listen(port,host, (req,res) => {
+    console.log('practice server is running',req,res)
+})
+
+//use express
